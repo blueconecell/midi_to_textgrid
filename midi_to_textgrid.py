b879d2275759d3174a397035a97ea1c5a91e71d0
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2022-08-10 18:15:46 +0900

    Update midi_to_textgrid.py

--- midi_to_textgrid.py
+++ midi_to_textgrid.py
@@ -1,62 +1,74 @@
-from mido import MidiFile
+import mido
 import textgrid
 
-mid = MidiFile("2022-06-29_2차_야생화_key-Ab-01(T69).mid")
-tg = textgrid.TextGrid.fromFile('2022-06-29_2차_야생화_key-Ab-01_T69_.TextGrid')
-def Data_creater(midi_file):
-    midimsgs = []
-    for i in midi_file:
-        if i.type == 'note_on' or i.type == 'note_off':
-            midimsgs.append(i.dict())
+midi_fname = input('midi 파일명을 입력해주세요 : ')
+tg_fname = input('TextGrid 파일명을 입력해주세요 : ')
+write_name = input('결과물 파일명을 입력해주세요 : ')
+midi_fname = midi_fname +'.mid'
+tg_fname = tg_fname +'.TextGrid'
+write_name = write_name +'.TextGrid'
 
-    mem1 = 0
-    for i in midimsgs:
-        time = i['time'] + mem1
-        i['time'] = time
-        mem1 = i['time']
-    return midimsgs
-tg[2].__init__('int', 0.0, tg.maxTime)
-
-
-def createTG_for_intervalTier(Data):
-    max_time = Data[-1]['time']
-    data_len = len(Data)
-
-    # body part
-    time = ["0", ]
-    text = ["", ]
-
-    body = ""
-    for i in range(data_len):
-        data = Data[i]
-
-        if data['type'] == 'note_on':
-            text.append(data['note'])
-            time.append(data['time'])
-
-        elif data['type'] == 'note_off':
-            text.append("")
-            time.append(data['time'])
-
+note_scale = {}
+for i in range(40, 100):
+    if i%12==0:
+        note_scale[i]=f'C{i//12 - 2}'
+    elif i%12==1:
+        note_scale[i]=f'Db{i//12 - 2}'
+    elif i%12==2:
+        note_scale[i]=f'D{i//12 - 2}'
+    elif i%12==3:
+        note_scale[i]=f'Eb{i//12 - 2}'
+    elif i%12==4:
+        note_scale[i]=f'E{i//12 - 2}'
+    elif i%12==5:
+        note_scale[i]=f'F{i//12 - 2}'
+    elif i%12==6:
+        note_scale[i]=f'Gb{i//12 - 2}'
+    elif i%12==7:
+        note_scale[i]=f'G{i//12 - 2}'
+    elif i%12==8:
+        note_scale[i]=f'Ab{i//12 - 2}'
+    elif i%12==9:
+        note_scale[i]=f'A{i//12 - 2}'
+    elif i%12==10:
+        note_scale[i]=f'Bb{i//12 - 2}'
+    elif i%12==11:
+        note_scale[i]=f'B{i//12 - 2}'
+        
+tg = textgrid.TextGrid.fromFile(tg_fname)
+mid = mido.MidiFile(midi_fname)
+msg = []
+for i in mid:
+    if i.type=='note_on'or i.type=='note_off':
+        msg.append(i.dict())
+time=[]
+note=[]
+for i in msg[1::2]:
+    time.append(i['time'])
+for i in msg:
+    if i['type'] == 'note_on':
+        eng_note = note_scale[i['note']]
+        note.append(eng_note)
+        
+time_accumulate = []
+stacking = msg[0]['time']
+for i in time: 
+    if i > 0.01:
+        time_accumulate.append(stacking)
+    stacking +=i    
+    
+def tg_creater(time_accumulate, note):
+    time=[0.0]
+    time = time + time_accumulate
+    
+    tg[2].__init__('int', 0.0, tg.maxTime)
+    
     time_len = len(time)
-    intervals_no = 1
-
-    #     for i,j in zip(time, text):
-    #         print(i,j)
-
-    # time 부분을 직접 넣어서 돌리는 방법으로 교체
-    for i in range(time_len - 1):
-        if abs(float(time[i]) - float(time[i + 1])) < 0.02:
-            pass
-        #           print('skipped :',time[i], time[i+1], text[i])
-        else:
-            tg[2].add(float(time[i]), float(time[i + 1]), f"{text[i]}")
-
-# main
-
-Data = Data_creater(mid)
-createTG_for_intervalTier(Data)
-
-
-
-tg.write("midi_to_textgrid_result.TextGrid")
+    
+    for i in range(time_len-2):
+        tg[2].add(float(time[i+1]), float(time[i+2]), f"{note[i]}") 
+        
+        
+#main
+tg_creater(time_accumulate,note)
+tg.write(write_name)
